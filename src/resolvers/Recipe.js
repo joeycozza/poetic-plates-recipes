e286bb465de785b208c84847cb253546f7b4cc71
Joey Cozza <joeycozza@gmail.com>
2023-10-10 17:07:35 -0700

    add Cookware entity to Recipe

--- src/resolvers/Recipe.js
+++ src/resolvers/Recipe.js
@@ -1,11 +1,19 @@
 module.exports = {
   Recipe: {
     ingredients(recipe, _, { dataSources }) {
-      return dataSources.recipesAPI.getRecipeIngredients(recipe.id);
+      return dataSources.recipesAPI.getRecipeIngredients(recipe.id)
     },
     relatedRecipes(recipe, _, { dataSources }) {
-      const limit = 3;
-      return dataSources.recipesAPI.getRelatedRecipes(recipe.id, limit);
+      const limit = 3
+      return dataSources.recipesAPI.getRelatedRecipes(recipe.id, limit)
+    },
+    cookware(recipe, _, { dataSources }) {
+      const cookwareNamesList = dataSources.recipesAPI.getRecipeCookware(recipe.id)
+      if (!cookwareNamesList) return
+
+      return cookwareNamesList.map((c) => ({
+        name: c,
+      }))
     },
   },
-};
+}
